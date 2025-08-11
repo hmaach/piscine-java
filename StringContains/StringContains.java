22badf12f7adc64796ed364a6e5493880b8958ed
Hamza Maach <hamzamaach56@gmail.com>
2025-08-11 15:47:58 +0100

    check if a string contains a substring

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -2,6 +2,8 @@
 public class StringContains {
 
     public static boolean isStringContainedIn(String subString, String s) {
+        if (s == null) s = "";
+        if (subString == null) subString = "";
 
         return s.contains(subString);
     }
