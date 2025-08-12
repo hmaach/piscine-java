4a8a330d919e7a2cd9bfd802fa2b41fc4a5f97a7
Hamza Maach <hamzamaach56@gmail.com>
2025-08-12 14:24:48 +0100

    return true if the String in parameter is a palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -4,6 +4,6 @@ public class Palindrome {
     public static boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder(s);
         sb.reverse();
-        return s.equals(sb.toString());
+        return s.toLowerCase().equals(sb.toString().toLowerCase());
     }
 }
