a0e48c79b3ddc23534712c11e5b1d4964361d3d7
Hamza Maach <hamzamaach56@gmail.com>
2025-08-12 14:43:01 +0100

    compute each int element of an array

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,21 @@
+
+public class ComputeArray {
+
+    public static int[] computeArray(int[] array) {
+        int[] new_array = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            int nb = array[i];
+            if (nb % 3 == 0) {
+                new_array[i] = nb * 5;
+            } else {
+                if ((nb % 3) % 2 == 0) {
+                    new_array[i] = nb;
+                } else if ((nb % 3) % 1 == 0) {
+                    new_array[i] = nb + 7;
+                }
+            }
+        }
+
+        return new_array;
+    }
+}
