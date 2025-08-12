a9726524bc02b69221a631aae13f13fda7191f84
Hamza Maach <hamzamaach56@gmail.com>
2025-08-12 15:44:12 +0100

    compute each int element of an array

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,19 +1,24 @@
-// import java.util.Arrays;
+
 public class ComputeArray {
 
     public static int[] computeArray(int[] array) {
+        if (array == null) {
+            return null;
+        }
         int[] new_array = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             int nb = array[i];
             int mod = nb % 3;
 
             switch (mod) {
-                case 0 -> new_array[i] = nb * 5;
-                case 1 -> new_array[i] = nb + 7;
-                default -> new_array[i] = nb;
+                case 0 ->
+                    new_array[i] = nb * 5;
+                case 1 ->
+                    new_array[i] = nb + 7;
+                default ->
+                    new_array[i] = nb;
             }
         }
         return new_array;
     }
-
 }
