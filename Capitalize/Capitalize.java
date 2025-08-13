0d26f8403cbc477154775b9709b6ff61ed6fe6fc
Hamza Maach <hamzamaach56@gmail.com>
2025-08-13 11:14:56 +0100

    read from a text from a file and capitalize it before write it into the output file

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -1,7 +1,5 @@
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Capitalize {
@@ -11,16 +9,13 @@ public class Capitalize {
             throw new IOException("invalid number of args");
         }
 
-        String content = Files.readString(Paths.get(args[0]));
-        System.out.println(content);
-
         File file = new File(args[0]);
         try (Scanner in = new Scanner(file); OutputStream out = new FileOutputStream(args[1])) {
             while (in.hasNext()) {
                 String word = in.next();
 
                 String firstLetter = word.substring(0, 1).toUpperCase();
-                String restOfWord = word.substring(1);
+                String restOfWord = word.substring(1).toLowerCase();
                 String capitalized = firstLetter + restOfWord;
 
                 out.write(capitalized.getBytes());
