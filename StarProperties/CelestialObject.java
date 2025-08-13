92ee1e9d34e749de1ec35e02a6ab08543fe023c3
Hamza Maach <hamzamaach56@gmail.com>
2025-08-13 16:56:31 +0100

    add constractors to the class

--- StarProperties/CelestialObject.java
+++ StarProperties/CelestialObject.java
@@ -1,3 +1,4 @@
+// package StarProperties;
 
 public class CelestialObject {
 
@@ -6,4 +7,17 @@ public class CelestialObject {
     public double z;
     public String name;
 
+    public CelestialObject() {
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+        name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
 }
