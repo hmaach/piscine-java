9bf7c95127870b1b6d0acb6badd453159e9b4afc
Hamza Maach <hamzamaach56@gmail.com>
2025-08-15 13:49:22 +0100

    create Star class that inherits from CelestialObject class

---
+++ StarInheritance/Star.java
@@ -0,0 +1,14 @@
+package StarInheritance;
+
+public class Star extends CelestialObject {
+
+    private double magnitude;
+
+    public double getMagnitude() {
+        return this.magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+}
