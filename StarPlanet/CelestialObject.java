e7b76a98b04ef23b871f1daa27f8bb5efae2a3e8
Hamza Maach <hamzamaach56@gmail.com>
2025-08-15 15:02:39 +0100

    Create a new class Planet that inherits CelestialObject class

---
+++ StarPlanet/CelestialObject.java
@@ -0,0 +1,94 @@
+// package StarPlanet;
+
+import java.util.Objects;
+
+public class CelestialObject {
+
+    protected double x;
+    protected double y;
+    protected double z;
+    protected String name;
+
+    public static double KM_IN_ONE_AU = 150_000_000.;
+
+    public CelestialObject() {
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+        name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static double getDistanceBetween(CelestialObject defaultStar, CelestialObject earth) {
+
+        double dx = defaultStar.getX() - earth.getX();
+        double dy = defaultStar.getY() - earth.getY();
+        double dz = defaultStar.getZ() - earth.getZ();
+
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject defaultStar, CelestialObject earth) {
+        return CelestialObject.getDistanceBetween(defaultStar, earth) * CelestialObject.KM_IN_ONE_AU;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is positioned at (%.3f, %.3f, %.3f)", this.name, this.x, this.y, this.z);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        CelestialObject other = (CelestialObject) obj;
+        return this.x == other.getX() && this.y == other.getY() && this.z == other.getZ() && this.name.equals(other.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name, this.x, this.y, this.z);
+    }
+}
