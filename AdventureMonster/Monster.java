f883452cc19be421c4c596cd1a376ac4f30fb27c
Hamza Maach <hamzamaach56@gmail.com>
2025-08-18 16:03:28 +0100

    create Monster class that extends from Character class

---
+++ AdventureMonster/Monster.java
@@ -0,0 +1,17 @@
+// package AdventureMonster;
+
+public class Monster extends Character {
+
+    public Monster(String name, int maxHealth) {
+        super(name, maxHealth);
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth == 0) {
+            return String.format("%s is a monster and is dead", this.name);
+        } else {
+            return String.format("%s is a monster with %d HP", this.name, this.currentHealth);
+        }
+    }
+}
