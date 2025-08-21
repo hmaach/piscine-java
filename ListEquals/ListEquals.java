eafeb5987f963cb0337b91916bd320baa906a1f1
Hamza Maach <hamzamaach56@gmail.com>
2025-08-21 15:39:34 +0100

    check if two lists are equal

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -4,6 +4,9 @@ import java.util.List;
 public class ListEquals {
 
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
+        if (list1 == null || list2 == null) {
+            return list1 == list2;
+        }
         return list1.equals(list2);
     }
 }
