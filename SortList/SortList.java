7196ad6df533f44f5a352c65815947c205a01579
Hamza Maach <hamzamaach56@gmail.com>
2025-08-21 18:31:34 +0100

    sort a list using stream

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -6,10 +6,16 @@ import java.util.stream.Collectors;
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
         return list.stream().sorted().collect(Collectors.toList());
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
         return list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 }
