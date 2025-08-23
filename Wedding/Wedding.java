a2bb3306cc20054f0f9d5a3226f555812602e798
Hamza Maach <hamzamaach56@gmail.com>
2025-08-23 15:14:50 +0100

    associates randomly a name from the first list to a name of the second list into a map

--- Wedding/Wedding.java
+++ Wedding/Wedding.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,25 +11,16 @@ public class Wedding {
     public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
         Map<String, String> couples = new HashMap<>();
 
-        List<String> biggerList;
-        List<String> smallerList;
-
         List<String> firstList = new ArrayList<>(first);
         List<String> secondList = new ArrayList<>(second);
 
-        if (first.size() > second.size()) {
-            biggerList = firstList;
-            smallerList = secondList;
-        } else {
-            biggerList = secondList;
-            smallerList = firstList;
-        }
+        Collections.shuffle(firstList);
+        Collections.shuffle(secondList);
 
-        for (int i = 0; i < smallerList.size(); i++) {
-            String firstSingle = biggerList.get(i);
-            String secondSingle = smallerList.get(i);
+        int size = Math.min(firstList.size(), secondList.size());
 
-            couples.put(firstSingle, secondSingle);
+        for (int i = 0; i < size; i++) {
+            couples.put(firstList.get(i), secondList.get(i));
         }
 
         return couples;
