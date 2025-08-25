4d49eee13f8391f96d3a69c1aa0ab481c2822b6f
Hamza Maach <hamzamaach56@gmail.com>
2025-08-25 17:40:45 +0100

    get the duration between two times using time.Duration

--- DifferenceBetweenDate/DifferenceBetweenDate.java
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -8,14 +8,23 @@ import java.time.Period;
 public class DifferenceBetweenDate {
 
     public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
+        if (localTime1 == null || localTime2 == null) {
+            return null;
+        }
         return Duration.between(localTime1, localTime2);
     }
 
     public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+        if (date1 == null || date2 == null) {
+            return null;
+        }
         return Period.between(date1, date2);
     }
 
     public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        if (dateTime1 == null || dateTime2 == null) {
+            return null;
+        }
         return Math.abs(Duration.between(dateTime1, dateTime2).toHours());
     }
 
