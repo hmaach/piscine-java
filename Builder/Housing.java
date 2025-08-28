629b473717f29cbcdf168d3bb20a3af5552457e3
Hamza Maach <hamzamaach56@gmail.com>
2025-08-28 16:17:28 +0100

    aplly builder design pattern

---
+++ Builder/Housing.java
@@ -0,0 +1,11 @@
+
+public interface Housing {
+
+    public void setSize(int size);
+
+    public void setPrice(int price);
+
+    public void setRooms(int rooms);
+
+    public void setName(String name);
+}
